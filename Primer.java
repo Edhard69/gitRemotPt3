60c2a2555461edb604bcc4aa06b4ac5fc3e2b3e7
Edgar <eaguila@almata.cat>
2022-09-27 16:29:00 +0200

    Pujar els arxius primer i segon modificats amb el super programa

--- Primer.java
+++ Primer.java
@@ -1,2 +1,4 @@
 //Primer arxiu creat, encara no he fet cap commit
 //Aquest és el segon comentari, per a modificar una mica aquest l'arxiu
+
+//El programa serà increible
--- Segon.java
+++ Segon.java
@@ -1,1 +1,3 @@
 //És un nou arxiu per a millorar el nostre programa.
+
+//Canvis per a un programa increible!!
